dcb02e01dccdcfb9bf5b1c472cb7707d5986a131
borkild <65369831+borkild@users.noreply.github.com>
2023-09-17 14:04:17 -0600

    Started Gini Index Function

--- DecisionTree/DT_Practice.py
+++ DecisionTree/DT_Practice.py
@@ -95,7 +95,38 @@ def ME(data):
 
 # function to calculate Gini Index
 def GI(data):
-    pass
+    dataShape = data.shape
+    # start by calculating overall entropy
+    labels = np.unique(data[:,dataShape[1]-1]) # find unique labels
+    print(labels.size)
+    num_label = np.empty((labels.size))
+    for labelIdx in range(len(labels)): # find amount of each label
+        num_label[labelIdx] = np.sum(data[:,dataShape[1]-1] == labels[labelIdx])
+    GI_S = np.sum(-1*(num_label/dataShape[0])*np.log2(num_label/dataShape[0])) # calculate overall entropy
+    attGain = np.empty(dataShape[1] - 1) # array to write entropy for each attribute to
+
+    for attIdx in range(dataShape[1] - 1): # iterate through columns of data array -- going through Attributes
+        att = np.unique(data[:,attIdx]) # get unique attribute values
+        numAttVal = np.empty(len(att)) # array to store number of each attribute value
+        H_S_v = np.empty(len(att)) # array to store entropy of each attribute value, H(S_v)
+
+        for attValIdx in range(len(att)): # iterate through specific values of a single attribute
+            attVal_Loc = np.argwhere(data[:,attIdx] == att[attValIdx])
+            numAttVal[attValIdx] = attVal_Loc.size
+            attLabel = np.empty((labels.size)) 
+
+            for labelIdx in range(len(labels)): # iterate through labels for specific attribute value
+                attLabel[labelIdx] = np.sum(data[attVal_Loc,dataShape[1]-1] == labels[labelIdx])
+            # calculate entropy for attribute subset
+            zeroIdx = np.argwhere(attLabel == 0) # need to adjust for 0 entries to aviod -inf*0 = nan
+            if np.any(zeroIdx):
+                attLabel[attLabel == 0] = numAttVal[attValIdx]
+                H_S_v[attValIdx] = np.sum(-1*(attLabel/numAttVal[attValIdx])*np.log2(attLabel/numAttVal[attValIdx]))
+            else: 
+                H_S_v[attValIdx] = np.sum(-1*(attLabel/numAttVal[attValIdx])*np.log2(attLabel/numAttVal[attValIdx]))
+        # now calculat information gain for each attribute
+        attGain[attIdx] = H_S - np.sum((numAttVal/len(data[:,attIdx]))*H_S_v)
+    return attGain
 
 # class to create our trees
 
