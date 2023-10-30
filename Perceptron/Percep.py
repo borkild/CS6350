bd5d329e00a77c16bebecbb4c0e12c9fd5228678
Ben Orkild <ben.orkild@utah.edu>
2023-10-30 17:38:32 -0600

    Implemented standard perceptron, appears to be working

--- Perceptron/Percep.py
+++ Perceptron/Percep.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import numpy as np
 
 # now import functions from custom decision tree implementation
@@ -15,15 +16,47 @@ def convertLabel(labels):
 
 
 # function to generate standard perceptron weights
-def trainPerceptron(trainData, numEpochs, r):
-    pass
-
+def trainPerceptron(trainData, trainLabels, numEpochs = 100, learnRate = 1.0):
+    #initialize weights, we include the bias in our weight vector, so we add 1 additional parameter to it
+    w = np.zeros(trainData.shape[1] + 1)
+    # initialize list to track training accuracy
+    trainAcc = []
+    # iterate through each epoch
+    for epoch in range(numEpochs):
+        # shuffle data and labels
+        shuffIdx = list(range(trainData.shape[0]))
+        random.shuffle(shuffIdx)
+        data = trainData[shuffIdx,:]
+        labels = trainLabels[shuffIdx]
+        # iterate through training examples
+        for exIdx in range(data.shape[0]):
+            x_i = np.append(data[exIdx, :], 1) # current row of data
+            y_i = labels[exIdx] # current label
+            updateW = np.dot(w*y_i, x_i)
+            if updateW <= 0: # update weights 
+                w = w + learnRate*y_i*x_i
+    
+    # return weights and accuracies
+    return w
 
 
 
 # function to do forward pass through perceptron with weights
-def percepForward():
-    pass
+def percepStandForward(w, data, labels):
+    output = np.zeros(labels.shape)
+    for exIdx in range(data.shape[0]): # iterate through data rows
+        x_i = np.append(data[exIdx,:], 1) # grab current example
+        out = np.dot(w, x_i) # calculate output from model
+        if out < 0: # update output array based on sign of out
+            output[exIdx] = -1
+        else:
+            output[exIdx] = 1
+
+    acc = np.sum(labels == output)/labels.size
+
+    return output, acc
+        
+        
 
 
 if __name__ == "__main__":
@@ -44,10 +77,19 @@ if __name__ == "__main__":
     testLabels = convertLabel(testLabels) # convert labels from 1 or 0 to 1 or -1
     testData = np.delete(testData, testData.shape[1]-1, axis=1) # delete labels from training array
 
-    # now we apply the perceptron algorithm to our data
+    # now we apply the standard perceptron algorithm to our data
+    trainError = []
+    testError = []
+    for numEpoch in range(1,11):
+        weights = trainPerceptron(trainData, trainLabels, numEpoch, learnRate=0.1)
+        out, trainAcc = percepStandForward(weights, trainData, trainLabels)
+        trainError.append(1 - trainAcc)
+        out, testAcc = percepStandForward(weights, testData, testLabels)
+        testError.append(1 - testAcc)
 
 
-    ben = 1
+
+    # apply voted perceptron
     
     
 
