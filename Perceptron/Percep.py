cecb40af7d6dfe3de818868c39e106e85ac9b5d4
Ben Orkild <ben.orkild@utah.edu>
2023-10-30 18:09:01 -0600

    added voted perceptron, seems to work correctly

--- Perceptron/Percep.py
+++ Perceptron/Percep.py
@@ -39,6 +39,38 @@ def trainPerceptron(trainData, trainLabels, numEpochs = 100, learnRate = 1.0):
     # return weights and accuracies
     return w
 
+# function to train voted perceptron
+def trainVotedPercep(trainData, trainLabels, numEpochs = 100, learnRate = 1.0):
+    #initialize weights, we include the bias in our weight vector, so we add 1 additional parameter to it
+    w = np.zeros(trainData.shape[1] + 1)
+    # initialize list to track w and c
+    wList = []
+    cList = []
+    # iterate through each epoch
+    for epoch in range(numEpochs):
+        # shuffle data and labels
+        shuffIdx = list(range(trainData.shape[0]))
+        random.shuffle(shuffIdx)
+        data = trainData[shuffIdx,:]
+        labels = trainLabels[shuffIdx]
+        c = 1
+        # iterate through training examples
+        for exIdx in range(data.shape[0]):
+            x_i = np.append(data[exIdx, :], 1) # current row of data
+            y_i = labels[exIdx] # current label
+            updateW = np.dot(w*y_i, x_i)
+            if updateW <= 0: # update weights 
+                w = w + learnRate*y_i*x_i
+                wList.append(w)
+                cList.append(c)
+                c = 1
+            else:
+                c = c + 1
+    
+    # return weights and accuracies
+    return wList, cList
+
+
 
 
 # function to do forward pass through perceptron with weights
@@ -56,7 +88,23 @@ def percepStandForward(w, data, labels):
 
     return output, acc
         
-        
+# function to do forward pass on voted perceptron
+def percepVotedForward(w, c, data, labels):
+    output = np.zeros(labels.shape)
+    for exIdx in range(data.shape[0]): # iterate through data rows
+        out = 0
+        for wIdx in range(len(w)): # iterate through weight vectors
+            x_i = np.append(data[exIdx,:], 1) # grab current example
+            w_i = w[wIdx]
+            out = np.dot(w_i, x_i)*c[wIdx] + out # calculate output from model
+        if out < 0: # update output array based on sign of out
+            output[exIdx] = -1
+        else:
+            output[exIdx] = 1
+
+    acc = np.sum(labels == output)/labels.size
+
+    return output, acc
 
 
 if __name__ == "__main__":
@@ -78,23 +126,30 @@ if __name__ == "__main__":
     testData = np.delete(testData, testData.shape[1]-1, axis=1) # delete labels from training array
 
     # now we apply the standard perceptron algorithm to our data
-    trainError = []
-    testError = []
+    trainErrorST = []
+    testErrorST = []
     for numEpoch in range(1,11):
         weights = trainPerceptron(trainData, trainLabels, numEpoch, learnRate=0.1)
         out, trainAcc = percepStandForward(weights, trainData, trainLabels)
-        trainError.append(1 - trainAcc)
+        trainErrorST.append(1 - trainAcc)
         out, testAcc = percepStandForward(weights, testData, testLabels)
-        testError.append(1 - testAcc)
+        testErrorST.append(1 - testAcc)
 
 
 
     # apply voted perceptron
+    trainErrorV = []
+    testErrorV = []
+    for numEpoch in range(1,11):
+        weights, c = trainVotedPercep(trainData, trainLabels, numEpoch, learnRate=0.1)
+        out, trainAcc = percepVotedForward(weights, c, trainData, trainLabels)
+        trainErrorV.append(1 - trainAcc)
+        out, testAcc = percepVotedForward(weights, c, testData, testLabels)
+        testErrorV.append(1 - testAcc)
     
-    
 
 
-
+ben = 1
 
 
 
