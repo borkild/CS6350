6ccbc3d7bb4f05c2ff9b244f6d78262847a9d118
Ben Orkild <ben.orkild@utah.edu>
2023-12-16 13:27:20 -0700

    finished NN try

--- FinalProject/RealSubmissions.py
+++ FinalProject/RealSubmissions.py
@@ -9,6 +9,7 @@ from sklearn import ensemble
 from sklearn import svm
 import torch
 import torchvision
+import torchinfo
 
 # now import functions from custom decision tree implementation -- we only use this to load in the data
 # we assume our function is called from the CS6350 folder, which should occur if we run from the run script
@@ -39,6 +40,56 @@ def catToNum(data, attributes, attVals):
 
 
 # define simple network architecture
+class Layer3NN(torch.nn.Module):
+    def __init__(self):
+        super(Layer3NN, self).__init__()
+        self.Layers = torch.nn.Sequential(
+            torch.nn.Linear(14, 10),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10, 15),
+            torch.nn.Dropout(p=0.33),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.33),
+            torch.nn.Linear(15, 15),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.33),
+            torch.nn.Linear(15, 10),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.33),
+            torch.nn.Linear(10, 10),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.33),
+            torch.nn.Linear(10, 10),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.33),
+            torch.nn.Linear(10, 1),
+            torch.nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        out = self.Layers(x)
+        return out
+
+# define pytorch class for dataset
+class SalPredict(torch.utils.data.Dataset):
+    def __init__(self, dataArray, dataLabels, split):
+        self.split = split
+        # make all data 32 bit float point
+        self.data = np.float32(dataArray) # should be a numpy array
+        self.labels = np.expand_dims(np.float32(dataLabels), axis=1) # should also be a numpy array
+
+    def __len__(self):
+        return self.labels.size
+    
+    def __getitem__(self, idx):
+        inputData = torch.from_numpy(self.data[idx,:]) # convert to torch tensor here
+        label = torch.tensor(self.labels[idx])
+        return inputData, label
+
+
+def init_Custom_Weight(m):
+    if isinstance(m, torch.nn.Linear):
+        torch.nn.init.kaiming_uniform_(m.weight)
 
 
 
@@ -90,6 +141,7 @@ if __name__ == "__main__":
     # for now we just convert numeric labels to binary based on being above or below the median
     #trainData = DT.convertNumData(trainData, attributes)
     # now replace categorical attributes with numeric label
+    print("Converting Training Data")
     trainDataNum = catToNum(trainData, attributes, attribVals)
 
     # repeat for test data
@@ -100,22 +152,27 @@ if __name__ == "__main__":
     IDs = testData[:, 0]
     testData = np.delete(testData, (0), axis=1)
     # now convert test data to numeric values
+    print("Converting Testing Data")
     testDataNum = catToNum(testData, attributes, attribVals)
 
 
 
-    # split training into train and validation sets
-    numVal = int(round(trainDataNum.shape[0]*0.10)) # will use 10% of training data for validation set
-    idxList = list(range(trainDataNum.shape[0])) 
-    valIdx = random.sample(idxList, numVal) # indices to use as validation data
+    # split training into train and validation sets -- make validation set have even number of each label
+    numVal0 = int(round(trainDataNum.shape[0]*0.05))
+    numVal1 = int(round(trainDataNum.shape[0]*0.05)) # will use 10% of training data for validation set
+    idx0 = np.argwhere(trainLabels == 0)
+    idx1 = np.argwhere(trainLabels == 1)
+    idxList0 = list(range(idx0.shape[0])) 
+    idxList1 = list(range(idx1.shape[0]))
+    valIdx0 = random.sample(idxList0, numVal0) # indices to use as validation data
+    valIdx1 = random.sample(idxList1, numVal1)
     # write validation set to new array
+    valIdx = np.append(idx0[valIdx0], idx1[valIdx1])
     valDataNum = trainDataNum[valIdx, :]
     valLabels = trainLabels[valIdx]
     # delete validation set from training data
     trainDataNum = np.delete(trainDataNum, valIdx, axis=0)
     trainLabels = np.delete(trainLabels, valIdx)
-    
-
 
 
     outFields = ['ID', 'Prediction']
@@ -202,7 +259,7 @@ if __name__ == "__main__":
         plt.savefig("FinalProject/figures/BagAcc.png")
         plt.close
 
-    tstSVM = False
+    tstSVM = True
     if tstSVM:
         kernList = ["linear", "poly", "rbf"]
         for k in range(len(kernList)):
@@ -251,7 +308,7 @@ if __name__ == "__main__":
             writeOutput(outFileName, final_output, outFields) 
 
 
-    RBFtry = False
+    RBFtry = True
     if RBFtry:
         # initialize SVM model with desired kernel
             SVMmod = svm.SVC(kernel="rbf", gamma='auto', C=10)
@@ -298,9 +355,227 @@ if __name__ == "__main__":
 
 
     
-    tstNN = True
+    tstNN = False
     if tstNN:
-        pass 
+        print(np.sum(trainLabels))
+        print(np.size(trainLabels))
+        # check for gpu
+        if torch.cuda.device_count() == 0: # if no GPU was found, throw error
+            raise Exception("No GPU found")
+        print(torch.cuda.get_device_name(0))
+        # initialize pytorch dataloaders -- we use these because they take care of shuffling the data through each epoch for us
+        trainDataset = SalPredict(trainDataNum, trainLabels, split='train')
+        valDataset = SalPredict(valDataNum, valLabels, split='Validation')
+        trainDataLoader = torch.utils.data.DataLoader(trainDataset, batch_size=trainDataNum.shape[0], shuffle=True)
+        valDataLoader = torch.utils.data.DataLoader(valDataset, batch_size=valDataNum.shape[0], shuffle=True)
+        # initialize model and send to gpu
+        simpleNN = Layer3NN()
+        simpleNN.apply(init_Custom_Weight)
+        simpleNN.cuda()
+        print(torchinfo.summary(simpleNN), (1, 14))
+        
+        # initialize optimizer
+        optimizer = torch.optim.Adam(simpleNN.parameters(), lr = 0.01)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)
+        # initialize loss function
+        loss = torch.nn.MSELoss()
+        # intialize other training parameters and arrays to hold values
+        numEpoch = 400
+        trainAcc = []
+        trainLoss = []
+        valAcc = []
+        valLoss = []
+        best_Acc = 0
+        for epoch in range(numEpoch): # iterate through epochs
+            simpleNN.train()
+            print("epoch " + str(epoch))
+            for batchIdx, (data, label) in enumerate(trainDataLoader): # iterate through batches -- in this case the gpu is big enough to load all samples at once
+                # zero the gradients
+                optimizer.zero_grad()
+                # send data and labels to gpu
+                data = data.cuda()
+                label = label.cuda()
+                # run data through model
+                output = simpleNN(data)
+                # compute loss
+                loss_val = loss(output, label)
+                # calculate gradients with backpropagation
+                loss_val.backward()
+                # update parameters with optimizer
+                optimizer.step() 
+            # get training and validation accuracy 
+            for batchIdx, (data,label) in enumerate(trainDataLoader):
+                simpleNN.eval()
+                with torch.no_grad():
+                    data = data.cuda()
+                    label = label.cuda()
+                    predict = simpleNN(data)
+                    lossV = loss(predict, label)
+                    predict = predict.cpu().detach().numpy() # bring prediction back from gpu memory
+                    label = label.cpu().detach().numpy()
+                    trainLoss.append(lossV.item())
+                    predict = np.float32(predict > 0.5)
+                    acc = np.sum(predict == label)/np.size(label)
+                    trainAcc.append(acc)
+                    print("Training Accuracy: {}".format(acc))
+
+            for batchIdx, (data,label) in enumerate(valDataLoader):
+                simpleNN.eval()
+                with torch.no_grad():
+                    data = data.cuda()
+                    label = label.cuda()
+                    predict = simpleNN(data)
+                    lossV = loss(predict, label)
+                    predict = predict.cpu().detach().numpy() # bring prediction back from gpu memory
+                    label = label.cpu().detach().numpy()
+                    predict = np.float32(predict > 0.5)
+                    acc = np.sum(predict == label)/np.size(label)
+                    valLoss.append(lossV.item())
+                    valAcc.append(acc)
+                    print("Validation Accuracy: {}".format(acc))
+                    if best_Acc < acc:
+                        best_Acc = acc
+                        tstData = torch.from_numpy(np.float32(testDataNum))
+                        tstData = tstData.cuda()
+                        tstPredict = simpleNN(tstData)
+                        tstPredict = tstPredict.cpu().detach().numpy()
+                        tstPredict = np.int32(tstPredict > 0.5)
+                        final_output = np.zeros((testDataNum.shape[0], 2))
+                        final_output[:,0] = IDs
+                        final_output[:,1] = np.squeeze(tstPredict)
+                        final_output = final_output.astype(int)
+                        outFileName = "FinalProject/Submissions/NN_3.csv"
+                        writeOutput(outFileName, final_output, outFields)
+                        
+            
+        epochs = list(range(numEpoch))
+        plt.plot(epochs, valLoss)
+        plt.plot(epochs, trainLoss)
+        plt.show()
+
+
+    tstNNrs = True
+    if tstNNrs:
+        print(np.sum(trainLabels))
+        print(np.size(trainLabels))
+        # check for gpu
+        if torch.cuda.device_count() == 0: # if no GPU was found, throw error
+            raise Exception("No GPU found")
+        print(torch.cuda.get_device_name(0))
+        # initialize pytorch dataloaders -- we use these because they take care of shuffling the data through each epoch for us
+        # find samples with <50k and >50k labels and split to form even classes
+        over50Idx = np.argwhere(trainLabels == 1)
+        under50Idx = np.argwhere(trainLabels == 0)
+        # swap samples with income <50k to address class imbalance
+        under50Samp = random.sample(list(range(under50Idx.size)), over50Idx.size)
+        newTrainIdx = np.concatenate((over50Idx, under50Idx[under50Samp]))
+        newtrainData = trainDataNum[newTrainIdx, :]
+        newtrainLabels = trainLabels[newTrainIdx]
+
+        trainDataset = SalPredict(newtrainData, newtrainLabels, split='train')
+        trainDataLoader = torch.utils.data.DataLoader(trainDataset, batch_size=newtrainData.shape[0], shuffle=True)
+
+        valDataset = SalPredict(valDataNum, valLabels, split='Validation')
+        valDataLoader = torch.utils.data.DataLoader(valDataset, batch_size=valDataNum.shape[0], shuffle=True)
+        # initialize model and send to gpu
+        simpleNN = Layer3NN()
+        simpleNN.apply(init_Custom_Weight)
+        simpleNN.cuda()
+        print(torchinfo.summary(simpleNN), (1, 14))
+        
+        # initialize optimizer
+        optimizer = torch.optim.Adam(simpleNN.parameters(), lr = 0.001)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
+        # initialize loss function
+        loss = torch.nn.MSELoss()
+        # intialize other training parameters and arrays to hold values
+        numEpoch = 2000
+        trainAcc = []
+        trainLoss = []
+        valAcc = []
+        valLoss = []
+        best_Acc = 0
+        for epoch in range(numEpoch): # iterate through epochs
+            # put network into training mode
+            simpleNN.train()
+            print("epoch " + str(epoch))
+            for batchIdx, (data, label) in enumerate(trainDataLoader): # iterate through batches -- in this case the gpu is big enough to load all samples at once
+                # zero the gradients
+                optimizer.zero_grad()
+                # send data and labels to gpu
+                data = data.cuda()
+                label = label.cuda()
+                # run data through model
+                output = simpleNN(data)
+                # compute loss
+                loss_val = loss(output, label)
+                # calculate gradients with backpropagation
+                loss_val.backward()
+                # update parameters with optimizer
+                optimizer.step() 
+            # get training and validation accuracy 
+            for batchIdx, (data,label) in enumerate(trainDataLoader):
+                simpleNN.eval()
+                with torch.no_grad():
+                    data = data.cuda()
+                    label = label.cuda()
+                    predict = simpleNN(data)
+                    lossV = loss(predict, label)
+                    predict = predict.cpu().detach().numpy() # bring prediction back from gpu memory
+                    label = label.cpu().detach().numpy()
+                    trainLoss.append(lossV.item())
+                    predict = np.float32(predict > 0.5)
+                    acc = np.sum(predict == label)/np.size(label)
+                    trainAcc.append(acc)
+                    print("Training Accuracy: {}".format(acc))
+
+            for batchIdx, (data,label) in enumerate(valDataLoader):
+                simpleNN.eval()
+                with torch.no_grad():
+                    data = data.cuda()
+                    label = label.cuda()
+                    predict = simpleNN(data)
+                    lossV = loss(predict, label)
+                    predict = predict.cpu().detach().numpy() # bring prediction back from gpu memory
+                    label = label.cpu().detach().numpy()
+                    predict = np.float32(predict > 0.5)
+                    acc = np.sum(predict == label)/np.size(label)
+                    valLoss.append(lossV.item())
+                    valAcc.append(acc)
+                    print("Validation Accuracy: {}".format(acc))
+                    if best_Acc < acc:
+                        best_Acc = acc
+                        tstData = torch.from_numpy(np.float32(testDataNum))
+                        tstData = tstData.cuda()
+                        tstPredict = simpleNN(tstData)
+                        tstPredict = tstPredict.cpu().detach().numpy()
+                        tstPredict = np.int32(tstPredict > 0.5)
+                        final_output = np.zeros((testDataNum.shape[0], 2))
+                        final_output[:,0] = IDs
+                        final_output[:,1] = np.squeeze(tstPredict)
+                        final_output = final_output.astype(int)
+                        outFileName = "FinalProject/Submissions/NN_3.csv"
+                        writeOutput(outFileName, final_output, outFields)
+                        
+            
+        epochs = list(range(numEpoch))
+        plt.figure()
+        plt.plot(epochs, valLoss, label="Validation")
+        plt.plot(epochs, trainLoss, label = "Training")
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.savefig('FinalProject/figures/NNLoss.png')
+
+        plt.figure()
+        plt.plot(epochs, valAcc, label = 'Validation')
+        plt.plot(epochs, trainAcc, label = "Training")
+        plt.xlabel("Epochs")
+        plt.ylabel("Accuracy")
+        plt.legend()
+        plt.savefig('FinalProject/figures/NNAcc.png')
+        
+
 
 
 
